5bbb0db344d54672d54348ce0fbe3b837ced635e
eduper1 <eduper.havard@gmail.com>
2022-07-17 09:37:23 +0300

    all entry and button are set

--- dataEntry.py
+++ dataEntry.py
@@ -8,7 +8,7 @@ window.geometry("700x250")
 window.resizable(width=False, height=False)
 
 # Create the book entry frame with an Entry
-frm_entry = tk.Frame(master=window)
+#frm_entry = tk.Frame(master=window)
 book_title = tk.Entry(width=10)
 author_Lname = tk.Entry(width=10)
 author_Other_name = tk.Entry(width=10)
@@ -18,8 +18,11 @@ subject = tk.Entry(width=10)
 lbl_submit = tk.Button(text="Submit")
 lbl_quit = tk.Button(text="Quit")
 
-author_Lname.grid(row=1, column=0, sticky="e")
-author_Other_name.grid(row=1, column=1, sticky="w")
+book_title.grid(row=0, column=0)
+author_Lname.grid(row=2, column=0)
+author_Other_name.grid(row=2, column=1)
+subject.grid(row=1, column=0)
+# subject.place(x=40, y= 20)
 
 lbl_submit.grid(row=3, column=0, sticky="e")
 lbl_quit.grid(row=3, column=1, sticky="w")
