05e5c50408861eb69988952b518731771f3fd133
eduper1 <eduper.havard@gmail.com>
2022-07-19 23:04:23 +0300

    data can be added as a list or tupple

--- dataxl.py
+++ dataxl.py
@@ -4,4 +4,13 @@ wb = load_workbook('books.xlsx')
 ws = wb.active
 print(ws['A1'].value)
 
+take_input1 = input(" write col A: ")
+take_input2 = input(" write col B: ")
+take_input3 = input(" write col C: ")
+take_input4 = input(" write col D: ")
+
+rows = (take_input1, take_input2, take_input3, take_input4)
+
+ws.append(rows)
+
 wb.save('books.xlsx')
