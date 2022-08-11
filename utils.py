848b2269dccbfc88121b32ff7ee73457ffa335f0
eduper1 <eduper.havard@gmail.com>
2022-08-11 20:31:43 +0300

    trying to connect to sqlite3 db

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import sqlite3
 from openpyxl import workbook, load_workbook
 
 # open active excel workbook
@@ -5,9 +6,19 @@ wb = load_workbook('books.xlsx')
 # open the active work sheet
 ws = wb.active
 
+ac_db = sqlite3.connect('acMombasa.db')
+ac_cursor = ac_db.cursor()
+
+ac_cursor.execute(
+    'create table books (book_title text, book_subject text, book_qty intger, author_Lname text, author_Oname text, book_status text, comment text, register_time text)'
+)
+
+
 def cal_qty(quantity, data_list):
     for i in range(1, quantity+1):
         ws.append(data_list)
+        ac_cursor.execute('insert into books values(?,?,?,?,?,?,?,?)', data_list)
         # save the workbook
         wb.save('books.xlsx')
+        ac_db.close()
 
