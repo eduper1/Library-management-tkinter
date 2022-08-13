baab183e49a5ebd7db172ce5f582edb19284f5e1
eduper1 <eduper.havard@gmail.com>
2022-08-13 14:16:49 +0300

    ...

--- settings.py
+++ settings.py
@@ -1,4 +1,26 @@
+import sqlite3
+
 WIDTH = 1020
 HEIGHT = 600
 
-#  window.resizable(width=False, height=False)
+
+
+# connection = sqlite3.connect("gta.db")
+# cursor = connection.cursor()
+
+# cursor.execute('create table gta (rl_year integer, rl_name text, city text)')
+
+# relist = [
+#     (1992,"gta"," new guernesy"),
+#     (2314,"gta2","anywhere"),
+#     (1994,"gta3","liberty city"),
+# ]
+
+# cursor.executemany('insert into gta values(?,?,?)', relist)
+
+# for row in cursor.execute("select * from gta"):
+#     print(row)
+
+# print()
+
+# connection.close()
